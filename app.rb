698cca74cc9baefd30c31b414f4d8e2745e3926c
Lucas Weiblen <lucasweiblen@gmail.com>
2014-05-18 16:04:34 -0300

    Finish API

--- app.rb
+++ app.rb
@@ -15,18 +15,40 @@ class App < Sinatra::Base
 
     get '/:id' do |id|
       @movie = Movie.get(id)
-      json @movie
+      if @movie
+        json @movie
+      else
+        halt 404, 'Movie not found'
+      end
     end
 
     put '/:id' do |id|
-      @movie = Movie.get(id)
-      @movie.update(params[:movie])
-      json message: "Movie updated"
+      begin
+        @movie = Movie.get(id)
+        @movie.update(params[:movie])
+        halt 204
+      rescue
+        halt 404, 'Movie not updated'
+      end
     end
-
+      
     post do
       @movie = Movie.create(params[:movie])
-      json message: "Movie created"
+      if @movie
+        halt 201, "Movie created"
+      else
+        halt 404, "Movie not created"
+      end
+    end
+
+    delete '/:id' do |id|
+      begin
+        @movie = Movie.get(id)
+        @movie.destroy
+        halt 204
+      rescue 
+        halt 404, 'Movie not removed'
+      end
     end
   end
 end
